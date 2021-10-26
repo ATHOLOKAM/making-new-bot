82f0552bcbd63c8a6d32746bfa4d8d2c27803c83
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2021-10-26 16:17:22 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -331,7 +331,7 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🔰DEVOLPERS🔰', url="http://t.me/movietym4")
         ]
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
