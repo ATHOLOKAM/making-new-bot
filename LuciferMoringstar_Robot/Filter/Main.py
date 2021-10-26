e0908bd01bbc4e8ecdf55b1ac2c35fdebaf79b1e
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2021-10-26 17:29:26 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('Making Video', url=f'{TUTORIAL}')
+                    InlineKeyboardButton('🔰DEVOLPERS🔰', url="http://t.me/movietym4")
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
